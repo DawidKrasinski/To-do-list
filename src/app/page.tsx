4f42a62155668a7221edcd870ef0d2cb41d9316a
DawidKrasinski <dawidkrasinski14@gmail.com>
2024-11-14 22:43:51 +0100

    create task page  added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -22,11 +23,11 @@ export default function Home() {
         <div className="relative">
           <input
             type="text"
-            id="input"
+            id="searchInput"
             className="absolute foreground w-full h-12 mt-5 cursor-text"
           />
-          <label htmlFor="input" className="absolute mt-8 opacity-65">
-            <i className="fa-solid fa-magnifying-glas font-regu pl-2 pr-3"></i>
+          <label htmlFor="searchInput" className="absolute mt-8 opacity-65">
+            <i className="fa-solid fa-magnifying-glass pl-2 pr-3"></i>
             Search Task Here
           </label>
         </div>
@@ -39,9 +40,12 @@ export default function Home() {
       </div>
 
       <section className="w-full h-full flex justify-center items-end">
-        <button className="w-16 h-16 rounded-full bg-gradient-to-tl from-purple-400 to-pink-300 fixed mb-6 cursor-pointer">
+        <Link
+          href="/addTask"
+          className="w-16 h-16 rounded-full bg-gradient-to-tl from-purple-400 to-pink-300 fixed mb-6 cursor-pointer flex justify-center items-center"
+        >
           <i className="fa-solid fa-plus text-black text-2xl"></i>
-        </button>
+        </Link>
       </section>
     </>
   );
