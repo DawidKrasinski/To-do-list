187c8f6783aa4531c627b5e74d3b9120ae5251ee
DawidKrasinski <dawidkrasinski14@gmail.com>
2024-12-24 14:44:14 +0100

    test

--- src/app/components/schedule/schedule-component.tsx
+++ src/app/components/schedule/schedule-component.tsx
@@ -1,19 +1,40 @@
 import { Task } from "@/app/taskType";
+import { useEffect, useRef } from "react";
 
 interface ScheduleProps {
   task: Task;
   setTask: React.Dispatch<React.SetStateAction<Task>>;
 }
 
+function EditableArea({
+  value,
+  onChange,
+}: {
+  value: string;
+  onChange: (value: string) => void;
+}) {
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (!ref.current) return;
+    ref.current.innerHTML = value;
+  }, [value]);
+  return (
+    <div
+      contentEditable={true}
+      ref={ref}
+      onInput={(e) => onChange(e.currentTarget.innerText)}
+      className="bg-muted rounded-lg p-3 min-h-24 relative empty:after:content-['Description'] after:absolute after:top-3 after:left-3 after:text-muted-foreground/40"
+    ></div>
+  );
+}
+
 export function Schedule({ setTask, task }: ScheduleProps) {
   const handleInputChange = (
-    e: React.ChangeEvent<HTMLInputElement> | React.FormEvent<HTMLDivElement>,
+    e: React.ChangeEvent<HTMLInputElement>,
     field: keyof Task
   ) => {
-    const value =
-      e.currentTarget instanceof HTMLInputElement
-        ? e.currentTarget.value
-        : e.currentTarget.innerText;
+    const value = e.currentTarget.value;
     setTask((prevTask) => ({ ...prevTask, [field]: value }));
   };
 
@@ -30,13 +51,7 @@ export function Schedule({ setTask, task }: ScheduleProps) {
           className="bg-muted p-3 placeholder:text-muted-foreground/40 rounded-lg"
           placeholder="Name"
         />
-        <div
-          contentEditable={true}
-          onInput={(e) => handleInputChange(e, "description")}
-          className="bg-muted rounded-lg p-3 min-h-24 relative empty:after:content-['Description'] after:absolute after:top-3 after:left-3 after:text-muted-foreground/40"
-        >
-          {task.description}
-        </div>
+        <EditableArea value={task.description} />
       </section>
 
       <div className="flex gap-4">
